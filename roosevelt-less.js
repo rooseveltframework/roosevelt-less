b76914e7f0abdeb3904e29e7175a5324c0db4b47
Eric Newport <kethinov@gmail.com>
2015-06-12 16:11:22 -0400

    adding argv override

--- roosevelt-less.js
+++ roosevelt-less.js
@@ -1,7 +1,18 @@
 'use strict';
 
 var fs = require('fs'),
-    less = require('less');
+    less = require('less'),
+    compress = true,
+    yuicompress = true;
+
+process.argv.forEach(function (val, index, array) {
+  switch (val) {
+    case '-no-minify':
+      compress = false;
+      yuicompress = false;
+      break;
+  }
+});
 
 module.exports = {
   versionCode: function(app) {
@@ -9,12 +20,18 @@ module.exports = {
   },
 
   parse: function(app, fileName, callback) {
+
+    // override params from argv
+    if (app.get('params').cssCompiler.params) {
+      app.get('params').cssCompiler.params.compress = compress;
+    }
+    
     var parser = new less.Parser({
           paths: app.get('cssPath')
         }),
         opts = app.get('params').cssCompiler.params || {
-          compress: true,
-          yuicompress: true
+          compress: compress,
+          yuicompress: yuicompress
         };
 
     parser.parse(fs.readFileSync(app.get('cssPath') + fileName, 'utf8'), function(err, tree) {
