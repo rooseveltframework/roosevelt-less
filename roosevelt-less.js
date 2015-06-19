56cfc12174019c07d766358eb55cf5145cc124be
Eric Newport <kethinov@gmail.com>
2015-06-19 10:55:19 -0400

    suppress magnification with noMinify param

--- roosevelt-less.js
+++ roosevelt-less.js
@@ -9,13 +9,21 @@ module.exports = {
   },
 
   parse: function(app, fileName, callback) {
+    var defaultCompress = true;
+    
+    // disable minify if noMinify param is present in roosevelt
+    if (app.get('params').noMinify) {
+      defaultCompress = false;
+      app.get('params').cssCompiler.params.compress = defaultCompress;
+      app.get('params').cssCompiler.params.yuicompress = defaultCompress;
+    }
 
     var parser = new less.Parser({
           paths: app.get('cssPath')
         }),
         opts = app.get('params').cssCompiler.params || {
-          compress: true,
-          yuicompress: true
+          compress: defaultCompress,
+          yuicompress: defaultCompress
         };
 
     parser.parse(fs.readFileSync(app.get('cssPath') + fileName, 'utf8'), function(err, tree) {
