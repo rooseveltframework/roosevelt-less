9fbf8ee4bf53bfd56d1340fd3424229effb915ed
Troy Coutu <Autre31415@gmail.com>
2017-11-13 13:10:43 -0500

    API refactor

--- roosevelt-less.js
+++ roosevelt-less.js
@@ -1,62 +1,58 @@
 const fs = require('fs')
+const path = require('path')
 const LessPluginCleanCSS = require('less-plugin-clean-css')
 const less = require('less')
 
 module.exports = {
   versionCode: function (app) {
-    return '@' + app.get('params').versionedCssFile.varName + ': \'' + app.get('appVersion') + '\';\n'
+    return `@${app.get('params').versionedCssFile.varName}: '${app.get('appVersion')}';\n`
   },
 
-  parse: function (app, fileName, callback) {
-    let defaultCompress = true
+  parse: function (app, fileName) {
+    return new Promise((resolve, reject) => {
+      const params = app.get('params').cssCompiler
 
-    // LESS render options
-    let options = {
-      filename: app.get('cssPath') + fileName,
-      paths: app.get('cssPath')
-    }
-
-    // Clean-css options
-    let opts
-
-    // LESS clean-css plugin
-    let cleanCSSPlugin
-
-    // disable minify if noMinify param is present in roosevelt
-    if (app.get('params').noMinify) {
-      defaultCompress = false
-      app.get('params').cssCompiler.params.advanced = defaultCompress
-      app.get('params').cssCompiler.params.aggressiveMerging = defaultCompress
-    }
-
-    // Enable minification and compilation with less-plugin-clean-css
-    if (defaultCompress) {
-      opts = app.get('params').cssCompiler.params || {
-        advanced: defaultCompress,
-        aggressiveMerging: defaultCompress
+      // LESS render options
+      let options = {
+        paths: app.get('cssPath'),
+        filename: path.basename(fileName)
       }
 
-      // Re-interpret 'compress' param into valid clean-css options
-      if (opts.hasOwnProperty('compress')) {
-        opts.advanced = true
-        opts.aggressiveMerging = true
-        delete opts.compress
+      let lessInput = fs.readFileSync(path.join(app.get('cssPath'), fileName), 'utf8')
+
+      // Clean-css options
+      let opts
+
+      // LESS clean-css plugin
+      let cleanCSSPlugin
+
+      if (params.sourceMap) {
+        options = Object.assign(options, params.sourceMap)
       }
 
-      cleanCSSPlugin = new LessPluginCleanCSS(opts)
-      options.plugins = [cleanCSSPlugin]
-    }
+      // use clean-css plugin to minify CSS if noMinify param is false
+      if (!app.get('params').noMinify) {
+        opts = params.cleanCSS || {}
 
-    less.render(fs.readFileSync(options.filename, 'utf8'), options, function (err, tree) {
-      let newFile
-      let newCss
+        // support use of the compress param
+        if (params.compress) {
+          opts.advanced = true
+          opts.aggressiveMerging = true
+        }
 
-      if (!err) {
-        newFile = app.get('cssCompiledOutput') + fileName.replace('.less', '.css')
-        newCss = tree.css
+        cleanCSSPlugin = new LessPluginCleanCSS(opts)
+        options.plugins = [cleanCSSPlugin]
       }
 
-      callback(err, newFile, newCss)
+      less.render(lessInput, options, (err, output) => {
+        if (err) {
+          reject(err)
+        }
+        let newFile = fileName.replace('.less', '.css')
+        let newCSS = output.css
+
+        resolve([newFile, newCSS])
+      })
     })
   }
 }
